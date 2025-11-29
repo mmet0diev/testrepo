d4e55735ede9ba008be4821bb544be1c20ffbc6a
Max <30389732+mmet0diev@users.noreply.github.com>
2025-11-29 22:17:57 +0200

    Add files via upload

---
+++ autoinst.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install discord
+sudo apt install code
+sudo apt install wine
+sudo apt install lutris
+sudo apt install code
+sudo apt install wireshark
+sudo apt install aircrack-ng
+sudo apt install nmap
+sudo apt install shotcut
+sudo apt install brave
+sudo apt install steam
